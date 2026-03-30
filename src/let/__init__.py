4aa86e8dc4c6fa1e12aa3ffcd350273f52c6ec4c
Adam Karpierz <akarpierz@gmail.com>
2026-03-30 14:13:24 +0200

    Update documentation

--- src/let/__init__.py
+++
@@ -1,7 +0,0 @@
-# Copyright (c) 2016 Taylor Marks
-# Copyright (c) 2016 Adam Karpierz
-# SPDX-License-Identifier: MIT
-
-from .__about__ import * ; del __about__  # type: ignore[name-defined]  # noqa
-
-from ._let import * ; del _let  # type: ignore[name-defined]  # noqa
